c9c54ad59d6678659c38597053dad8ba048a2689
unknown <manjohs@gmail.com>
2015-11-25 01:12:23 -0600

    Web graph initial code

--- src/searchengine/common/Node.java
+++ src/searchengine/common/Node.java
@@ -1,5 +1,53 @@
 package searchengine.common;
 
+import java.net.URL;
+
 public class Node {
+	
+	private String id;
+	private URL url;
+	
+	public Node(URL url, String id) {
+		this.id = id;
+		this.url = url;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public URL getUrl() {
+		return url;
+	}
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node other = (Node) obj;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		return true;
+	}
+	
+	
 
 }
--- src/searchengine/common/WebGraph.java
+++ src/searchengine/common/WebGraph.java
@@ -1,5 +1,36 @@
 package searchengine.common;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class WebGraph {
+	
+	Set<Node> allNodes;
+	Set<Edge> allEdges;
+	
+	public WebGraph() {
+		allNodes = new HashSet<Node>();
+		allEdges = new HashSet<Edge>();
+	}
+	
+	public void addNode(Node n) {
+		allNodes.add(n);
+	}
+	
+	public void addEdge(Edge e) {
+		if(!allNodes.contains(e.getFrom()) || !allNodes.contains(e.getTo())) {
+			throw new 
+				IllegalArgumentException("Nodes for this edge are not in the graph");
+		}
+		allEdges.add(e);
+	}
+
+	public Set<Node> getAllNodes() {
+		return allNodes;
+	}
+
+	public Set<Edge> getAllEdges() {
+		return allEdges;
+	}
 
 }
